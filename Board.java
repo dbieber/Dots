bb646491e3d17562986c46c36d9a0c498b4025b8
David Bieber <dbieber@princeton.edu>
2012-04-07 16:24:40 -0400

    fix board typos and start gnode

--- Board.java
+++ Board.java
@@ -7,38 +7,38 @@ public class Board
     private class Box 
     {
         int index;
-        int up;
-        int down;
-        int left;
-        int right;
+        Box up;
+        Box down;
+        Box left;
+        Box right;
         
         Box(int index)
         {
             this.index = index;
             if (index == 0){
-                up == null;
-                down == null;
-                left == null;
-                right == null;
+                up = null;
+                down = null;
+                left = null;
+                right = null;
             }
             else{
                 if (index%5 == 0) {
-                    right = 0;
-                    left = index - 1;
+                    right = board[0];
+                    left = board[index - 1];
                 }
                 else {
-                    right = index + 1;
-                    if (index%5 == 1) left = 0;
-                    else left = index - 1;
+                    right = board[index + 1];
+                    if (index%5 == 1) left = board[0];
+                    else left = board[index - 1];
                 }
                 if (index <= 5) {
-                    top = 0;
-                    bottom = index + 5;
+                    up = board[0];
+                    down = board[index + 5];
                 }
                 else {
-                    top = index - 5;
-                    if (index > 20) bottom = 0;
-                    else bottom index + 5;
+                    up = board[index - 5];
+                    if (index > 20) down = board[0];
+                    else down = board[index + 5];
                 }
             }
         }
@@ -47,7 +47,7 @@ public class Board
     public Board(int[][] b)
     {
         board = new Box[26];
-        for (int i = 0; i < 26, i++) {
+        for (int i = 0; i < 26; i++) {
             board[i] = new Box(i);
         }
         int curr = 1;
@@ -66,9 +66,9 @@ public class Board
     public ArrayList<Box> threeSided() {
         ArrayList<Box> list = new ArrayList<Box>();
         for (Box b:board) {
-            numNull = 0;
+            int numNull = 0;
             if (b.up == null) numNull++;
-            if (b.down == null) numNul++;
+            if (b.down == null) numNull++;
             if (b.left == null) numNull++;
             if (b.right == null) numNull++;
             if (numNull == 3) list.add(b);
